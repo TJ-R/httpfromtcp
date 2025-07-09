06fe47020392ea13de408c7f56cecfd63e36a67f
Thomas Runyan <runyant13@gmail.com>
2025-07-09 17:00:46 -0400

    Added internal request package and updated udpsender

---
+++ cmd/udpsender/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	addr, err := net.ResolveUDPAddr("udp", "localhost:42069")
+
+	if err != nil {
+		log.Fatal("Error with listening on port")
+	}
+
+	con, err := net.DialUDP("udp", nil, addr)
+	defer con.Close()
+
+	if err != nil {
+		log.Fatal("Failed to Dial UDP")
+	}
+
+	reader :=  bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("> ")	
+		line, err := reader.ReadString('\n')
+
+		if err != nil {
+			log.Printf("Error when reading line: %v\n", err)
+		}
+
+		_, err = con.Write([]byte(line))
+		
+		if err != nil {
+			log.Printf("Error when writing: %v\n", err)
+		}
+	}
+}
